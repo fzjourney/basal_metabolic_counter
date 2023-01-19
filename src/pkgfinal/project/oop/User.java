748df11f4ce40ad72ca9f410af8fa7365149f5ee
Muhammad Feiza Zharfan Priyatnadi <100272742+fzjourney@users.noreply.github.com>
2023-01-19 21:46:11 +0700

    Add files via upload

---
+++ src/pkgfinal/project/oop/User.java
@@ -0,0 +1,11 @@
+package pkgfinal.project.oop;
+
+public class User {
+    String name, password, email;
+    
+    public User(String name, String password, String email) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+    }
+}
