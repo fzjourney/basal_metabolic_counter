a36fd7daff58062dc933589c9452cbe6c95da064
Muhammad Feiza Zharfan Priyatnadi <100272742+fzjourney@users.noreply.github.com>
2023-01-19 21:49:32 +0700

    Delete src directory

--- src/pkgfinal/project/oop/meal.java
+++
@@ -1,7 +0,0 @@
-package pkgfinal.project.oop;
-
-public class meal extends obj{
-    public meal(String name, int calories, Float carbs, Float fat, Float protein, Float fibre) {
-        super(name, calories, carbs, fat, protein, fibre);
-    }
-}
--- src/pkgfinal/project/oop/snack.java
+++
@@ -1,7 +0,0 @@
-package pkgfinal.project.oop;
-
-public class snack extends obj{
-    public snack(String name, int calories, Float carbs, Float fat, Float protein, Float fibre) {
-        super(name, calories, carbs, fat, protein, fibre);
-    }
-}
